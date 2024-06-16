f7f31f6e5f8acb6f7841c578b4c37bac7bc46fc0
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-06-16 14:39:47 +0100

    Problems from Hyperskill (Comparator)

---
+++ Sort users/src/main/java/org/example/UserComparator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class UserComparator implements Comparator<User> {
+    @Override
+    public int compare(User user1, User user2) {
+        return user1.getName().compareTo(user2.getName());
+    }
+}
