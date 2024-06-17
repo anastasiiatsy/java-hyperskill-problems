2cbd24e907640e96894478ab45630f808b50ab88
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-06-17 17:50:36 +0100

    Problems from Hyperskill (Streams)

---
+++ Count passwords/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(countPasswords(Arrays.stream(scanner.nextLine().split("\\s+"))));
+    }
+
+    public static long countPasswords(Stream<String> passwordStream) {
+        return passwordStream.count();
+    }
+}
